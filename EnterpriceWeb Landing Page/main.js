96d075935de9d85d959f1d47a36a283195b3c3e7
whonix2904 <113676978+whonix2904@users.noreply.github.com>
2024-01-01 21:07:53 +0530

    50% work completed

---
+++ EnterpriceWeb Landing Page/main.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const button = document.querySelector('.button');
+    const navLinks = document.querySelector('.nav-links');
+
+    button.addEventListener('click', function () {
+        navLinks.classList.toggle('show');
+    });
+});
